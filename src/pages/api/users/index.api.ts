4b884b3e6d200b0417e2e9df5703c364b15c6838
Gabriela Carniel <gabiicarniel@gmail.com>
2023-09-16 11:01:27 -0300

    add: cookies

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,15 +8,32 @@ export default async function handler(
 ) {
   if (req.method !== 'POST') {
     return res.status(405).end()
-  }
+  } // rever por que prisma aceita qualquer método na rota (prisma ou next?)
 
   const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already exists',
+    })
+  }
 
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
