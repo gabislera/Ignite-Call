251d026d96c82e69913818b87c9a001098d732bd
Gabriela Carniel <gabiicarniel@gmail.com>
2023-09-19 17:38:39 -0300

    add: replace useEffect with react-query

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,10 +6,11 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './styles'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import dayjs from 'dayjs'
 import { api } from '@/src/lib/axios'
 import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
 
 interface Availability {
   possibleTimes: number[]
@@ -18,28 +19,47 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState<Availability | null>(null)
-  const router = useRouter()
+  // const [availability, setAvailability] = useState<Availability | null>(null)
 
+  const router = useRouter()
+  const username = String(router.query.username)
   const isDateSelected = !!selectedDate
 
-  const username = String(router.query.username)
+  // useEffect(() => {
+  //   if (!selectedDate) {
+  //     return
+  //   }
 
-  useEffect(() => {
-    if (!selectedDate) {
-      return
-    }
+  //   api
+  //     .get(`/users/${username}/availability`, {
+  //       params: {
+  //         date: dayjs(selectedDate).format('YYYY-MM-DD'),
+  //       },
+  //     })
+  //     .then((response) => {
+  //       setAvailability(response.data)
+  //     })
+  // }, [selectedDate, username])
 
-    api
-      .get(`/users/${username}/availability`, {
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
+
+  const { data: availability } = useQuery<Availability>(
+    ['availability', selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
         params: {
-          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+          date: selectedDateWithoutTime,
         },
       })
-      .then((response) => {
-        setAvailability(response.data)
-      })
-  }, [selectedDate, username])
+
+      return response.data
+    },
+    {
+      enabled: !!selectedDate,
+    },
+  )
 
   const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
   const describedDate = selectedDate
