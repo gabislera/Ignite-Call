045206fb5afbfd99ee0ab18a94833f56c8eb9f1b
Gabriela Carniel <gabiicarniel@gmail.com>
2023-09-20 13:58:27 -0300

    feat: google api

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -16,8 +16,8 @@ export function buildNextAuthOptions(
         authorization: {
           params: {
             prompt: 'consent',
-            access_type: 'offline',
             response_type: 'code',
+            access_type: 'offline',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           }, // escopo de autorizações do google
